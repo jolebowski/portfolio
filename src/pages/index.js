761f92657dbaf8f070d9a1351567b7223ef1f524
jolebowski <jordan.moreira@epitech.eu>
2021-03-02 23:36:06 +0100

    create a navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import * as React from "react"
 
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+
 const IndexPage = () => {
   return (
+    <Layout>
+    <SEO title="Home" description="this is our home page" />
     <div>
       <h1>Coucou !!</h1>
     </div>
+    </Layout>
   )
 }
 
