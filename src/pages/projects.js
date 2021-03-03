23be2cfb340567522b296b0bb3393917a5cf47d5
jolebowski <jordan.moreira@epitech.eu>
2021-03-04 00:02:54 +0100

    edit file project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from "gatsby"
 
