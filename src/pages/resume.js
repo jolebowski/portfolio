9bce88b02ce8092a5a4b6e99fc7b11fca9142854
jolebowski <jordan.moreira@epitech.eu>
2021-03-04 11:50:38 +0100

    add page resume

---
+++ src/pages/resume.js
@@ -0,0 +1,131 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Resume = () => {
+  return (
+    <Layout>
+      <SEO title="Resume" />
+      <main>
+        <ul className="resume-categories">
+            <li className="resume-category">
+                <h2 className="resume-category-title">Cursus</h2>
+                <ol>
+                    <li className="resume-sub-category">
+                        <h3>2017 - 2020</h3>
+                        <h4>École Supérieure du Digital</h4>
+                        <h5>Bachelor Chargé de Projet Digital</h5>
+                        <ol className="resume-list">
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                        </ol>
+                    </li>
+                    <li className="resume-sub-category">
+                        <h3>2017 - 2020</h3>
+                        <h4>École Supérieure du Digital</h4>
+                        <h5>Bachelor Chargé de Projet Digital</h5>
+                        <ol className="resume-list">
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                        </ol>
+                    </li>
+                    <li className="resume-sub-category">
+                        <h3>2017 - 2020</h3>
+                        <h4>École Supérieure du Digital</h4>
+                        <h5>Bachelor Chargé de Projet Digital</h5>
+                        <ol className="resume-list">
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                        </ol>
+                    </li>
+                </ol>
+            </li>
+            <li className="resume-category">
+			<h2 className="resume-category-title">Cursus</h2>
+                <ol>
+                    <li className="resume-sub-category">
+                        <h3>2017 - 2020</h3>
+                        <h4>Lorem Ipsum</h4>
+                        <h5>Lorem Ipsum</h5>
+                        <ol className="resume-list">
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                        </ol>
+                    </li>
+                    <li className="resume-sub-category">
+                        <h3>2017 - 2020</h3>
+                        <h4>Lorem Ipsum</h4>
+                        <h5>Lorem Ipsum</h5>
+                        <ol className="resume-list">
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                        </ol>
+                    </li>
+                    <li className="resume-sub-category">
+                        <h3>2017 - 2020</h3>
+                        <h4>Lorem Ipsum</h4>
+                        <h5>Lorem Ipsum</h5>
+                        <ol className="resume-list">
+                            <li class="resume-item">Lorem Ipsum</li>
+                            <li class="resume-item">Lorem Ipsum</li>
+                            <li class="resume-item">Lorem Ipsum</li>
+                            <li class="resume-item">Lorem Ipsum</li>
+                        </ol>
+                    </li>
+                </ol>
+			</li>
+            <li className="resume-category">
+			<h2 className="resume-category-title">Cursus</h2>
+                <ol>
+                    <li className="resume-sub-category">
+                        <h3>2017 - 2020</h3>
+                        <h4>École Supérieure du Digital</h4>
+                        <h5>Bachelor Chargé de Projet Digital</h5>
+                        <ol className="resume-list">
+                            <li class="resume-item">Lorem Ipsum</li>
+                            <li class="resume-item">Lorem Ipsum</li>
+                            <li class="resume-item">Lorem Ipsum</li>
+                            <li class="resume-item">Lorem Ipsum</li>
+                        </ol>
+                    </li>
+                    <li className="resume-sub-category">
+                        <h3>2017 - 2020</h3>
+                        <h4>École Supérieure du Digital</h4>
+                        <h5>Bachelor Chargé de Projet Digital</h5>
+                        <ol className="resume-list">
+                            <li class="resume-item">Lorem Ipsum</li>
+                            <li class="resume-item">Lorem Ipsum</li>
+                            <li class="resume-item">Lorem Ipsum</li>
+                            <li class="resume-item">Lorem Ipsum</li>
+                        </ol>
+                    </li>
+                    <li className="resume-sub-category">
+                        <h3>2017 - 2020</h3>
+                        <h4>Lorem Ipsum</h4>
+                        <h5>Lorem Ipsum</h5>
+                        <ol className="resume-list">
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                            <li class="resume-item">Art Direction</li>
+                        </ol>
+                    </li>
+                </ol>
+			</li>
+        </ul>
+      </main>
+    </Layout>
+  )
+}
+
+export default Resume
