e6d68f57891e25bb69e5bce2dac1cbc4dd58db2a
jolebowski <jordan.moreira@epitech.eu>
2021-04-04 00:49:52 +0200

    add a header for page index

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,21 @@ const IndexPage = () => {
   return (
     <Layout>
     <SEO title="Home" description="this is our home page" />
+      <header className="header-fixed">
+        <div className="fixed-header animate-background">
+          <div className="header-container">
+            <div className="header-text">
+              <h1>
+                <p className="animate-text-1">Front End Developer,</p>
+                <p className="animate-text-2">Software achitect</p>
+              </h1>
+            </div>
+            <h2 className="animate-subtitle">
+              I build websites with a focus on responsive design and accessibility.
+            </h2>
+          </div>
+        </div>
+      </header>
     </Layout>
   )
 }
