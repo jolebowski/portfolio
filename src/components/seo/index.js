01dbfb23f07cc7a5dcf752d948781e8679185527
jolebowski <jordan.moreira@epitech.eu>
2021-04-08 23:09:44 +0200

    test meta for image

--- src/components/seo/index.js
+++ src/components/seo/index.js
@@ -18,16 +18,17 @@ const query = graphql`
 const SEO = ({ title, description }) => {
   const { site } = useStaticQuery(query)
   const { siteDesc, siteTitle, image, twitterUsername } = site.siteMetadata
+  console.log(image, image)
   return (
     <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
       <meta name="description" content={description || siteDesc} />
-      <meta name="image" content={image} />
+      <meta name="image" content="https://github.com/jolebowski/portfolio/blob/master/static/assets/portfolio.jpg" />
       {/* twitter cards */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:title" content={siteTitle} />
       <meta name="twitter:description" content={siteDesc} />
-      <meta name="twitter:image" content={image} />
+      <meta name="twitter:image" content="https://github.com/jolebowski/portfolio/blob/master/static/assets/portfolio.jpg" />
 
       <meta property="og:url" content="http://iamjomoreira.netlify.app/" />
       <meta property="og:type" content="website" />
